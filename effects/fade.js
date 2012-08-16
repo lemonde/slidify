4b4129a9a747bb5553cd7abf163ce3c0a5de3639
Greg Bergé <neoziro@gmail.com>
2012-08-16 16:04:22 +0200

    initial commit

---
+++ effects/fade.js
@@ -0,0 +1,32 @@
+define(["$"], function($)
+{
+	"use strict";
+	
+	var Fade = function(options)
+	{
+		this.fadeOutDuration = 400;
+		this.fadeInDuration = 2000;
+
+		$.extend(this, options);
+	};
+
+	Fade.prototype = {
+
+		animate : function(slider, callback)
+		{
+			$(slider.currentSlide.item).show().css("opacity", 1);
+			$(slider.currentSlide.item).show().css("opacity", 0.01);
+			
+			$(slider.lastSlide.item).animate({opacity: 0}, {
+				duration: this.fadeOutDuration,
+				complete : function()
+				{
+					$(slider.currentSlide.item).animate({opacity: 1}, {duration: this.fadeInDuration, complete: callback});
+				}
+			});
+			//
+		}
+	};
+
+	return Fade;
+});
