ca8fa4e0c25ae74d085ffb8d53b12cb24ee9739a
neoziro <neoziro@gmail.com>
2013-01-12 00:33:04 +0100

    jQuery deps, options

--- slidify.js
+++ slidify.js
@@ -1,13 +1,38 @@
 /*! slidify v0.0.2 lemonde.fr */
 
 (function(window, undefined) {
-	var slidify = {};
 
-	// Export slidify to the global object
-	window.slidify = slidify;
+	var build = function($) {
+		var slidify = {};
+
+		slidify.Slider = function(options) {
+			this.options = {
+				data: [], // JSON data or DOM objects
+				delay: 5000, // Time between slides in ms
+				loop: true, // Loop mode
+				startIndex: 0, // Start slide index
+				effect: null, // Effect
+				wrapper: null, // Wrapper
+				root: null // Root element
+			};
+
+			this.options = $.extend(this.options, options);
+		};
+
+		// Export slidify to the global object
+		window.slidify = slidify;
+
+		return slidify;
+	};
+	
 
 	// Expose slidify as an AMD module
-	if(typeof define === "function" && define.amd) {
-		define("slidify", function() { return slidify; });
+	if(typeof define === 'function' && define.amd) {
+		define('slidify', ['jquery'], function($) { return build($); });
 	}
+
+	if(typeof window.jQuery === "function") {
+		build(window.jQuery);
+	}
+
 })(window);
--- test/unit/slidify.js
+++ test/unit/slidify.js
@@ -1,6 +1,10 @@
 var expect = chai.expect
 , jQ = jQuery.noConflict();
 
+define('jquery', function() {
+  return jQ;
+});
+
 describe('slidify', function() {
 
   beforeEach(function(done) {
@@ -20,5 +24,41 @@ describe('slidify', function() {
         done();
       });
     });
+  });
+
+  describe('Slider', function() {
+    describe('should exist a Slider prototype', function() {
+      expect(window.slidify.Slider).to.be.a('function');
+    });
+
+    describe('default options', function() {
+      it('should contain an object options', function() {
+        var slider = new window.slidify.Slider();
+        expect(slider.options).to.be.an('object');
+      });
+
+      it('should contain all options', function() {
+        var slider = new window.slidify.Slider();
+
+        expect(slider.options.data).to.be.an('array');
+        expect(slider.options.delay).to.be.a('number');
+        expect(slider.options.loop).to.be.a('boolean');
+        expect(slider.options.startIndex).to.be.a('number');
+        expect(slider.options.effect).to.be.a('null');
+        expect(slider.options.wrapper).to.be.a('null');
+        expect(slider.options.root).to.be.a('null');
+      });
+    });
+
+    describe('extend options', function() {
+      it('should contain an object options extended', function() {
+        var slider = new window.slidify.Slider({
+          delay: 5555
+        });
+
+        expect(slider.options.delay).to.equal(5555);
+      });
+    });
+
   });
 });
