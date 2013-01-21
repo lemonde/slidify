f68185048a7b5310aa450a872127553b04316fac
neoziro <neoziro@gmail.com>
2013-01-21 17:30:19 +0100

    quote correction + jshint options

--- slidify.js
+++ slidify.js
@@ -31,7 +31,7 @@
     define('slidify', ['jquery'], function($) { return build($); });
   }
 
-  if(typeof window.jQuery === "function") {
+  if(typeof window.jQuery === 'function') {
     build(window.jQuery);
   }
 
